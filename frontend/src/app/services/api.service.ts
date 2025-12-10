65b8366a8f8d6fa7c0284b521097f2a7d77d638f
Aguinaldo Neto <110543106+netokemon@users.noreply.github.com>
2025-12-09 23:26:37 -0300

    Update base URL in ApiService

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -5,7 +5,7 @@ import { environment } from '../../environments/environment';
 @Injectable({ providedIn: 'root' })
 export class ApiService {
 
-  private baseUrl = 'https://transcare-api.onrender.com/auth';
+  private baseUrl = 'https://transcare-api.onrender.com/api';
 
   constructor(private http: HttpClient) {}
 
