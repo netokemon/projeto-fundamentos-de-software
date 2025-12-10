d19d72ea9ae3a04e05e4d46f9903f3422ae8b18f
Elis Tenório <elismaidi@gmail.com>
2025-12-09 22:41:59 -0300

    Add environment configuration files for production and development

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // Coloque a URL que você copiou do Backend no passo anterior (sem a barra no final)
+  apiUrl: 'https://transcare-api.onrender.com/'
+};
